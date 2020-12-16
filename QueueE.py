0577820b9850e82c683ff25292be43ede77be646
zjvalenciana <zjvalenciana@miners.utep.edu>
2020-12-15 22:03:39 -0600

    added mutex lock

--- QueueE.py
+++ QueueE.py
@@ -1,6 +1,7 @@
 import threading
 from threading import Semaphore
-
+from threading import Lock
+mutex = Lock()
 class QueueE: 
     def __init__(self, capacity = 10):
         self.queue = [] 
@@ -10,14 +11,19 @@ class QueueE:
 
     def enqueue(self, item):
         self.semaphoreCapacity.acquire()
+        mutex.acquire()
         self.queue.append(item)
+        mutex.release()
         self.semaphoreUsed.release()
 
     def dequeue(self):
         self.semaphoreUsed.acquire()
+        mutex.acquire()
         item = self.queue.pop(0)
+        mutex.release()
         self.semaphoreCapacity.release()
         return item
 
     def peek(self):
         return self.queue[0]
+
